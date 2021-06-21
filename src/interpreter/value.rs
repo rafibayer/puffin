64868bf47f0e9c15e6a747e1eb3183e549273491
Rafael Bayer <rafibayer7@gmail.com>
2021-06-21 00:37:46 -0700

    added nested environments

--- src/interpreter/value.rs
+++ src/interpreter/value.rs
@@ -1,21 +1,36 @@
+use std::cell::RefCell;
 use std::{collections::HashMap, rc::Rc};
 use super::node;
 use super::InterpreterError;
 
 
 pub struct Environment {
+    parent: Option<Rc<RefCell<Environment>>>,
     bindings: HashMap<String, Value>
 }
 
 impl Environment {
-    pub fn bind(&mut self, name: &str, value: Value) {
-        self.bindings.insert(name.to_string(), value);
+
+    pub fn new() -> Environment {
+        Environment {
+            parent: None,
+            bindings: HashMap::new(),
+        }
+    }
+
+    pub fn bind(&mut self, name: &str, value: &Value) {
+        self.bindings.insert(name.to_string(), value.clone());
     }
 
     pub fn get(&self, name: &str) -> Result<Value, InterpreterError> {
         match self.bindings.get(name) {
             Some(value) => Ok(value.clone()),
-            None => Err(InterpreterError::UnboundName(name.to_string())),
+            None => {
+                match &self.parent {
+                    Some(parent) => parent.borrow().get(name),
+                    None => Err(InterpreterError::UnboundName(name.to_string())),
+                }
+            },
         }
     }
 }
