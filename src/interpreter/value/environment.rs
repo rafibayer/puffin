1f84c0af086887dfdf4e961aa55538ad39ae1225
Rafael Bayer <rafibayer7@gmail.com>
2021-06-22 12:40:27 -0700

    prevent rebinding of builtins in environments

--- src/interpreter/value/environment.rs
+++ src/interpreter/value/environment.rs
@@ -1,45 +1,57 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{cell::RefCell, collections::{HashMap, HashSet}, rc::Rc};
 
-use super::{InterpreterError, Value};
+use super::{InterpreterError, Value, builtin};
 
 
 
 #[derive(Debug, Clone)]
 pub struct Environment {
+    // parent environment
     parent: Option<Rc<RefCell<Environment>>>,
-    bindings: HashMap<String, Value>
+    // local bindings
+    bindings: HashMap<String, Value>,
+    // builtin names, can't be rebound
+    builtins: HashSet<String>,
 }
 
 impl Environment {
-
     pub fn new() -> Environment {
+        let bindings = builtin::get_builtins();
+        let builtins = bindings.keys().cloned().collect();
         Environment {
             parent: None,
-            bindings: HashMap::new(),
+            bindings,
+            builtins
         }
     }
 
     pub fn new_inner(outer: Rc<RefCell<Environment>>) -> Environment {
+        let bindings = builtin::get_builtins();
+        let builtins = bindings.keys().cloned().collect();
         Environment {
             parent: Some(outer),
-            bindings: HashMap::new(),
+            bindings,
+            builtins
         }
     }
 
-    pub fn bind(&mut self, name: String, value: &Value) {
+    pub fn bind(&mut self, name: String, value: &Value) -> Option<InterpreterError> {
+        if self.builtins.contains(&name) {
+            return Some(InterpreterError::BuiltinRebinding(name));
+        }
         self.bindings.insert(name, value.clone());
-    }
 
+        None
+    }
 
     pub fn get(&self, name: String) -> Result<Value, InterpreterError> {
         match self.bindings.get(&name) {
             Some(value) => Ok(value.clone()),
             None => match self.parent {
-                    Some(ref env) => {
-                        env.borrow().get(name)
-                    },
-                    None => Err(InterpreterError::UnboundName(name)),
-                }
+                Some(ref env) => env.borrow().get(name),
+                None => Err(InterpreterError::UnboundName(name)),
+            },
         }
     }
+
 }
